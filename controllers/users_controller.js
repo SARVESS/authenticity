59a7d94fc36e4b674d88dd1c72df63ada1fbfb40
SARVESS <sarveshsri76@gmail.com>
2020-06-28 20:46:49 +0530

    Completed Sign In and Sign Up

---
+++ controllers/users_controller.js
@@ -0,0 +1,72 @@
+const User = require('../models/user');
+
+
+module.exports.profile = function(req, res){
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
+}
+
+
+// render the sign up page
+module.exports.signUp = function(req, res){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
+    return res.render('user_sign_up', {
+        title: "Authenticity | Sign Up"
+    })
+}
+
+
+// render the sign in page
+module.exports.signIn = function(req, res){
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Authenticity | Sign In"
+    })
+}
+
+// get the sign up data
+module.exports.create = function(req, res){
+    if (req.body.password != req.body.confirm_password){
+        req.flash('error', 'Passwords do not match!');
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('error in creating user while signing up'); return}
+                
+                req.flash('success', 'Signed Up Succesfully!');
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            req.flash('error', 'User Already exists!');
+            return res.redirect('back');
+        }
+
+    });
+}
+
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged In Successfully');
+    return res.redirect('/users/profile');
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+    req.flash('success', 'You have logged out!');
+    
+    return res.redirect('/');
+}
